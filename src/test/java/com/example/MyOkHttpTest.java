fb38f3f1b3d5bb8f0536f01f514ced366a45d99e
redmojo7 <caipeng5180@sina.cn>
2017-05-06 11:43:18 +0800

    commit OkHttp test

---
+++ src/test/java/com/example/MyOkHttpTest.java
@@ -0,0 +1,75 @@
+package com.example;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * MyOkHttpTest
+ *
+ * @author : Donald Cai
+ * @date :   2017/5/6.
+ */
+@Service
+@Transactional
+public class MyOkHttpTest {
+
+    private static final Logger log = LoggerFactory.getLogger(MyOkHttpTest.class);
+    private static final OkHttpClient client = new OkHttpClient();
+    private static final String url = "http://publicobject.com/helloworld.txt";
+    @Test
+    public void testExecute(){
+        Request request = new Request.Builder().url(url).build();
+        Response response;
+        try {
+            response = client.newCall(request).execute();
+            if(response != null && response.isSuccessful()) {
+                log.debug("code :" + String.valueOf(response.code()));
+                log.debug("response: " + response.body().string() );
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testEnqueue(){
+        Request request = new Request.Builder().url(url).build();
+        CountDownLatch latch = new CountDownLatch(1);
+        client.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+                    log.debug("onFailure :" + call.isExecuted());
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    log.debug("code :" + String.valueOf(response.code()));
+                    log.debug("response: " + response.body().string() );
+                    latch.countDown();
+                    log.debug("latch.countDown(),count:" + latch.getCount());
+                }
+            });
+
+
+        try {
+            log.debug("latch.wait() :start");
+            latch.wait();
+            log.debug("latch.wait() :end");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        //waiting
+        testExecute();
+    }
+}
