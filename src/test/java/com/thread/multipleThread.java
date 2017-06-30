9555bf1088ce355f264c0f09a5cb5be0c78ac021
redmojo7 <caipeng5180@sina.cn>
2017-06-30 14:33:12 +0800

    commit : multiple thread debug

---
+++ src/test/java/com/thread/multipleThread.java
@@ -0,0 +1,25 @@
+package com.thread;
+
+/**
+ * Created by Administrator on 2017/6/30.
+ */
+public class multipleThread {
+  public static void main(String[] args) throws InterruptedException {
+    new Thread("aaa") { // 断点0
+      @Override
+      public void run() {
+        System.out.println("1"); // 断点1
+        try {
+          Thread.sleep(2000);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+        System.out.println("2"); // 断点2
+      }
+    }.start();
+    // 外线程
+    System.out.println("3"); // 断点3
+    Thread.sleep(2000);
+    System.out.println("4"); // 断点4
+  }
+}
