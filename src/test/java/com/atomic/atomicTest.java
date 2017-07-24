686ef3de10cbd711988c0ae8b16097db5b2fb27d
redmojo7 <caipeng5180@sina.cn>
2017-07-24 08:54:59 +0800

    commit : atomicReference

--- src/test/java/com/atomic/atomicTest.java
+++ src/test/java/com/atomic/atomicTest.java
@@ -1,5 +1,10 @@
 package com.atomic;
 
+import com.strat.springboot.Controller.domain.User;
+import java.util.concurrent.atomic.AtomicIntegerArray;
+import java.util.concurrent.atomic.AtomicReference;
+import org.junit.Test;
+
 /**
  * Created by Administrator on 2017/7/24.
  */
@@ -18,7 +23,38 @@ public class atomicTest {
         System.out.println("double to long bits  = " + l1);
         double d2 = Double.longBitsToDouble(l1);
         System.out.println("long bits to double = " + d2);
+        
+        
+    }
     
+    static int[] value = new int[] {1,2,3};
+    static AtomicIntegerArray ai = new AtomicIntegerArray(value);
+    
+    @Test
+    public void atomicArrayTest() {
+        ai.getAndSet(0,3);
+        System.out.println("ai = " + ai.get(0));
+        System.out.println("ai = " + value[0]);
+    }
+    
+    public static AtomicReference<User> atomicUserRef = new AtomicReference<>();
+    
+    @Test
+    public void atomicReference() {
+        
+        User user = new User("lucy", 24);
+        atomicUserRef.set(user);
+        User userUpdate = new User("donald", 25);
+        atomicUserRef.compareAndSet(user, userUpdate);
+        System.out.println("user.name = " + atomicUserRef.get().getName());
+        System.out.println("user.age = " + atomicUserRef.get().getAge());
+    
+        atomicUserRef.set(user);
+        User userUpdate2 = new User("lily", 24);
+        
+        atomicUserRef.compareAndSet(userUpdate, userUpdate2);
+        System.out.println("user.name = " + atomicUserRef.get().getName());
+        System.out.println("user.age = " + atomicUserRef.get().getAge());
         
     }
 }
